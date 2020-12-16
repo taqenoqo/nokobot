a4b59f26a2bf1fb3206b8f1d95873af9186cce1d
takenoko <ttaakkee@gmail.com>
2020-12-17 04:52:22 +0900

    Adjust for deploy

--- line/app.rb
+++ line/app.rb
@@ -28,10 +28,6 @@ post '/callback' do
           text: event.message['text']
         }
         client.reply_message(event['replyToken'], message)
-      when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-        response = client.get_message_content(event.message['id'])
-        tf = Tempfile.open("content")
-        tf.write(response.body)
       end
     end
   end
